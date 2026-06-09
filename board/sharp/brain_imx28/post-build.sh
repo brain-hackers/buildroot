ddab5277eaa64113c2c533e1371e324b02511594
Mingyang Li <tslmy@outlook.com>
2026-06-09 07:13:21 -0700

    fixup! configs: brain_imx28: enable haveged for early entropy on boot

--- board/sharp/brain_imx28/post-build.sh
+++ board/sharp/brain_imx28/post-build.sh
@@ -8,9 +8,17 @@ append_inittab()
 	patch --forward -r - -p 0 < board/sharp/brain_imx28/inittab.patch || true
 }
 
+promote_haveged_initscript()
+{
+	# haveged initializes at S21 by default. Move it to S10 so it starts before
+	# S20seedrng, which blocks on getrandom(2) until the CRNG is seeded.
+	mv "${TARGET_DIR}/etc/init.d/S21haveged" "${TARGET_DIR}/etc/init.d/S10haveged"
+}
+
 main()
 {
 	append_inittab
+	promote_haveged_initscript
 	exit $?
 }
 
